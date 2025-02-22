8ed6851a1653d525bd6750a0a3cb71b36699ad11
Galindo <anthony.galindo.506@my.csun.edu>
2025-02-21 18:16:37 -0800

    New models

---
+++ NewDatabaseModel/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewDatabaseModel;
+
+public partial class City
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    public int Populatoion { get; set; }
+
+    [Column("CountryID")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
